aa901c4e6dbff57bcfe23f14998be9cdfbd1cb00
nvarg <natvargas97@gmail.com>
2019-10-07 19:09:58 -0700

    Added Source files

---
+++ src/utils.h
@@ -0,0 +1,24 @@
+#include <string>
+
+
+// event
+struct input {};
+
+// guards
+const auto is_help = (const input&)[]{};
+const auto is_myport = (const input&)[]{};
+const auto is_myip = (const input&)[]{};
+const auto is_connect = (const input&)[]{};
+const auto is_list = (const input&)[]{};
+const auto is_terminate = (const input&)[]{};
+const auto is_send = (const input&)[]{};
+const auto is_exit = (const input&)[]{};
+
+const auto help = (const input&)[]{};
+const auto myport = (const input&)[]{};
+const auto myip = (const input&)[]{};
+const auto connect = (const input&)[]{};
+const auto list = (const input&)[]{};
+const auto terminate = (const input&)[]{};
+const auto send = (const input&)[]{};
+const auto exit = (const input&)[]{};
